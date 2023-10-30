93cca39cfd03b245d9829d1b60d72518a6cd64c7
JonatanNs <nsualu.jonatan@gmail.com>
2023-10-30 15:49:21 +0000

    ex5, exercice5

---
+++ ex_shell/ex5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+mkdir exercice5
+
+cd exercice5 
+
+mkdir dc && mkdir Marvel
+
+touch dc/superman.txt && touch dc/batman.txt
+
+touch Marvel/iron_man.txt && touch Marvel/captaine_america.txt 
+
+echo "Clark Kent" > dc/superman.txt && echo "Bruce Wayne" > dc/batman.txt
+
+echo"Steve Rogers" > Marvel/captain_america.txt && echo "Tony Stark" > Marvel/iron_man.txt
